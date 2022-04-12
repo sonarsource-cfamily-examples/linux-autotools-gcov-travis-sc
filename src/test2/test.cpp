a992c34d8eb0ec5eaea3f86801c112d955ab6f27
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2022-04-12 17:17:37 +0200

    Add more files and compute coverage

---
+++ src/test2/test.cpp
@@ -0,0 +1,14 @@
+#include "mylib/lib.h"
+
+int ff(int x) {
+  return 2 / x;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 5) {
+    return myfun(false, ff) == 0;
+  } else {
+    argc = myfun(false, ff);
+    return argc == 0;
+  }
+}
