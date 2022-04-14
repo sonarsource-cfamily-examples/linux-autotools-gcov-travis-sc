fb4fca1615a601435cd702595257ee255cb24c39
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2022-04-14 12:10:14 +0200

    fix test2 exit code

--- src/test2/test.cpp
+++ src/test2/test.cpp
@@ -6,7 +6,7 @@ int ff(int x) {
 
 int main(int argc, char *argv[]) {
   if (argc < 5) {
-    return myfun(false, ff) == 0;
+    return myfun(false, ff) != 0;
   } else {
     argc = myfun(false, ff);
     return argc == 0;
