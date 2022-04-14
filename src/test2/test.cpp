ced11b766d1970aa5840129e135bd68001269dbe
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2022-04-14 12:16:27 +0200

    simplify test2 to get 100% coverage here

--- src/test2/test.cpp
+++ src/test2/test.cpp
@@ -4,11 +4,6 @@ int ff(int x) {
   return 2 / x;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 5) {
-    return myfun(false, ff) != 0;
-  } else {
-    argc = myfun(false, ff);
-    return argc == 0;
-  }
+int main() {
+  return myfun(false, ff) != 0;
 }
